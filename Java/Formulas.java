30569f3c9d1a657a969c0d7056221cc860c4ca47
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2019-02-14 22:31:59 -0500

    Update Formulas.java

--- Java/Formulas.java
+++ Java/Formulas.java
@@ -1,3 +1,20 @@
+/**
+*@author AEK
+*@version 1.0
+*/
+import java.lang.Math;
 public class Formulas {
+  /**
+  *Returns the distance between two points who are passed in as their individual x and y components
+  *@param x1 The x component of the first point
+  *@param x2 The y component of the first point
+  *@param y1 The x component of the second point
+  *@param y2 The y component of the second point
+  *@return The distance between the two points
+  */
+  double distance(double x1, double x2, double y1, double y2) {
+    return Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1), 2));
+  }
+  
   
 }
